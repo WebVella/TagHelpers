5af15c42ac7a37e5a88e3350f427a8d41a861c08
Rumen Yankov <rumen.yankov@gmail.com>
2019-12-30 19:59:24 +0200

    remove embedded file provider usage

--- WebVella.TagHelpers/WebVellaTagHelpersConfigurationOptions.cs
+++ WebVella.TagHelpers/WebVellaTagHelpersConfigurationOptions.cs
@@ -1,41 +1,41 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.StaticFiles;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.Options;
-using System;
+﻿//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.StaticFiles;
+//using Microsoft.Extensions.FileProviders;
+//using Microsoft.Extensions.Options;
+//using System;
 
-namespace WebVella.TagHelpers
-{
-	internal class WebVellaTagHelpersConfigurationOptions : IPostConfigureOptions<StaticFileOptions>
-	{
-		private readonly IWebHostEnvironment _environment;
+//namespace WebVella.TagHelpers
+//{
+//	internal class WebVellaTagHelpersConfigurationOptions : IPostConfigureOptions<StaticFileOptions>
+//	{
+//		private readonly IWebHostEnvironment _environment;
 
-		public WebVellaTagHelpersConfigurationOptions(IWebHostEnvironment environment)
-		{
-			_environment = environment;
-		}
+//		public WebVellaTagHelpersConfigurationOptions(IWebHostEnvironment environment)
+//		{
+//			_environment = environment;
+//		}
 
-		public void PostConfigure(string name, StaticFileOptions options)
-		{
+//		public void PostConfigure(string name, StaticFileOptions options)
+//		{
 
-			//// Basic initialization in case the options weren't initialized by any other component
-			//options.ContentTypeProvider = options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
+//			//// Basic initialization in case the options weren't initialized by any other component
+//			//options.ContentTypeProvider = options.ContentTypeProvider ?? new FileExtensionContentTypeProvider();
 
-			//if (options.FileProvider == null && _environment.WebRootFileProvider == null)
-			//{
-			//	throw new InvalidOperationException("Missing FileProvider.");
-			//}
+//			//if (options.FileProvider == null && _environment.WebRootFileProvider == null)
+//			//{
+//			//	throw new InvalidOperationException("Missing FileProvider.");
+//			//}
 
-			//options.FileProvider = options.FileProvider ?? _environment.WebRootFileProvider;
+//			//options.FileProvider = options.FileProvider ?? _environment.WebRootFileProvider;
 
 
-			// Add our provider
-			//var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, "wwwroot");
-			//options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
-		}
-	}
-}
+//			// Add our provider
+//			//var filesProvider = new ManifestEmbeddedFileProvider(GetType().Assembly, "wwwroot");
+//			//options.FileProvider = new CompositeFileProvider(options.FileProvider, filesProvider);
+//		}
+//	}
+//}
 
 
 
--- WebVella.TagHelpers/WebVellaTagHelpersServiceCollectionExtensions.cs
+++ WebVella.TagHelpers/WebVellaTagHelpersServiceCollectionExtensions.cs
@@ -1,13 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿//using Microsoft.Extensions.DependencyInjection;
 
 
-namespace WebVella.TagHelpers
-{
-	public static class WebVellaTagHelpersServiceCollectionExtensions
-	{
-		public static void AddWebVellaTagHelpers(this IServiceCollection services)
-		{
-			services.ConfigureOptions(typeof(WebVellaTagHelpersConfigurationOptions));
-		}
-	}
-}
+//namespace WebVella.TagHelpers
+//{
+//	public static class WebVellaTagHelpersServiceCollectionExtensions
+//	{
+//		public static void AddWebVellaTagHelpers(this IServiceCollection services)
+//		{
+//			services.ConfigureOptions(typeof(WebVellaTagHelpersConfigurationOptions));
+//		}
+//	}
+//}
