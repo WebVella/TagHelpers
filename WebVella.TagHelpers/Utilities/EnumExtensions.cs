aa030a023cd5d408aad179ff2bd7e745597c88a6
Bozhidar Zashev <bzashev@users.noreply.github.com>
2023-05-15 13:37:47 +0300

    extending enum utils

--- WebVella.TagHelpers/Utilities/EnumExtensions.cs
+++ WebVella.TagHelpers/Utilities/EnumExtensions.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
+using System.Reflection;
 using System.Text;
+using WebVella.TagHelpers.Models;
 
 namespace WebVella.TagHelpers.Utilities
 {
@@ -27,5 +29,46 @@ namespace WebVella.TagHelpers.Utilities
 
 			return description;
 		}
-	}
+
+        public static List<TEnum> GetList<TEnum>() where TEnum : Enum
+        {
+            List<TEnum> selectEnumOptions = new();
+
+            if (!typeof(TEnum).IsEnum)
+            {
+                return selectEnumOptions;
+            }
+
+            foreach (FieldInfo field in typeof(TEnum).GetFields())
+            {
+                if (field.FieldType.IsEnum)
+                {
+                    selectEnumOptions.Add((TEnum)field.GetValue(0));
+                }
+            }
+
+            return selectEnumOptions;
+        }
+
+        public static List<WvSelectOption> GetAsSelectOptions<TEnum>() where TEnum : Enum
+        {
+            List<WvSelectOption> selectEnumOptions = new();
+
+            if (!typeof(TEnum).IsEnum)
+            {
+                return selectEnumOptions;
+            }
+
+            foreach (var value in Enum.GetValues(typeof(TEnum)))
+            {
+                selectEnumOptions.Add(new WvSelectOption
+                {
+                    Value = ((int)value).ToString(),
+                    Label = ((TEnum)value).ToDescriptionString()
+                });
+            }
+
+            return selectEnumOptions;
+        }
+    }
 }
